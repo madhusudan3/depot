e4e922aa2df96f84ebe1144475c75e8d0088edc0
Madhu <madhusudan.koukutla@gmail.com>
2018-08-21 15:35:24 -0500

    Third commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+Product.create(title: 'Harry Potter'),
+				description: %{
+					Harry Potter is a series of fantasy novels written by British author J. K. Rowling.
+				},
+				image_url: 'image1.jpg',
+				price: 25.95)
+
+Product.create(title: 'Naruto'),
+				description: %{
+					Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto.
+					It tells the story of Naruto Uzumaki, an adolescent ninja who searches for recognition from his peers and the village.
+				},
+				image_url: 'image2.jpg',
+				price: 06.99)
+
+Product.create(title: 'One Pice'),
+				description: %{
+					One Piece is a Japanese manga series written and illustrated by Eiichiro Oda.
+					It has been serialized in Shueisha's Weekly Shōnen Jump magazine since July 22, 1997, and has been collected in 89 tankōbon volumes.
+				},
+				image_url: 'image3.png',
+				price: 5.678)
