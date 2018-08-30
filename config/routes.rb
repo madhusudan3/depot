aa3c0c78dacc5d1f4306a4c4221aadbcc6bb5399
Madhu <madhusudan.koukutla@gmail.com>
2018-08-30 09:24:22 -0500

    tenth commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
   #get 'store/index'
   resources :products do
-  	get :who_bought, on: :menber
+  	get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
