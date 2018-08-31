e2bd193c7eb1df5df167240e62adc7d855aa0576
Madhu <madhusudan.koukutla@gmail.com>
2018-08-31 10:37:44 -0500

    eleventh commit

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -4,7 +4,7 @@ class OrderMailerTest < ActionMailer::TestCase
   test "received" do
     mail = OrderMailer.received(orders(:one))
     assert_equal "Anime Store Order Confirmation", mail.subject
-    assert_equal ["madhu@example.org"], mail.to
+    assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
     assert_match /1 x Programming Ruby 2.5.1/, mail.body.encoded
   end
@@ -12,7 +12,7 @@ class OrderMailerTest < ActionMailer::TestCase
   test "shipped" do
     mail = OrderMailer.shipped(orders(:one))
     assert_equal "Anime Store Order Shipped", mail.subject
-    assert_equal ["madhu@example.org"], mail.to
+    assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
     assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 2.5.1<\/td>/, mail.body.encoded
   end
